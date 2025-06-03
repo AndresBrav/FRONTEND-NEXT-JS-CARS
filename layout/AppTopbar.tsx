935e0576cd46945b712009ebe9dceedca3eb824c
Andres Bravo <bravoandres1706@gmail.com>
2025-06-03 13:10:28 -0400

    handle log out

--- layout/AppTopbar.tsx
+++ layout/AppTopbar.tsx
@@ -5,12 +5,14 @@ import { classNames } from 'primereact/utils';
 import React, { forwardRef, useContext, useImperativeHandle, useRef } from 'react';
 import { AppTopbarRef } from '@/types';
 import { LayoutContext } from './context/layoutcontext';
+import { TokenContext } from '@/app/(main)/context/TokenContext';
 
 const AppTopbar = forwardRef<AppTopbarRef>((props, ref) => {
     const { layoutConfig, layoutState, onMenuToggle, showProfileSidebar } = useContext(LayoutContext);
     const menubuttonRef = useRef(null);
     const topbarmenuRef = useRef(null);
     const topbarmenubuttonRef = useRef(null);
+    const { keyAccess, setKeyAccess } = useContext(TokenContext);
 
     useImperativeHandle(ref, () => ({
         menubutton: menubuttonRef.current,
@@ -18,24 +20,45 @@ const AppTopbar = forwardRef<AppTopbarRef>((props, ref) => {
         topbarmenubutton: topbarmenubuttonRef.current
     }));
 
+    const handleLogOut = () => {
+        setKeyAccess('');
+        localStorage.removeItem('token');
+    };
+
     return (
         <div className="layout-topbar">
             <Link href="/" className="layout-topbar-logo">
-                <img src={`/layout/images/car-${layoutConfig.colorScheme !== 'light' ? 'white' : 'dark'}.svg`} width="47.22px" height={'35px'} alt="logo" />
+                <img
+                    src={`/layout/images/car-${layoutConfig.colorScheme !== 'light' ? 'white' : 'dark'}.svg`}
+                    width="47.22px"
+                    height={'35px'}
+                    alt="logo"
+                />
                 <span>CARS</span>
             </Link>
-
-            <button ref={menubuttonRef} type="button" className="p-link layout-menu-button layout-topbar-button" onClick={onMenuToggle}>
+            <button
+                ref={menubuttonRef}
+                type="button"
+                className="p-link layout-menu-button layout-topbar-button"
+                onClick={onMenuToggle}
+            >
                 <i className="pi pi-bars" />
-            </button> {/* menu symbol */}
-
-            
-
-            <button ref={topbarmenubuttonRef} type="button" className="p-link layout-topbar-menu-button layout-topbar-button" onClick={showProfileSidebar}>
+            </button>{' '}
+            {/* menu symbol */}
+            <button
+                ref={topbarmenubuttonRef}
+                type="button"
+                className="p-link layout-topbar-menu-button layout-topbar-button"
+                onClick={showProfileSidebar}
+            >
                 <i className="pi pi-ellipsis-v" />
             </button>
-
-            <div ref={topbarmenuRef} className={classNames('layout-topbar-menu', { 'layout-topbar-menu-mobile-active': layoutState.profileSidebarVisible })}>
+            <div
+                ref={topbarmenuRef}
+                className={classNames('layout-topbar-menu', {
+                    'layout-topbar-menu-mobile-active': layoutState.profileSidebarVisible
+                })}
+            >
                 {/* <button type="button" className="p-link layout-topbar-button">
                     <i className="pi pi-calendar"></i>
                     <span>Calendar</span>
@@ -50,6 +73,10 @@ const AppTopbar = forwardRef<AppTopbarRef>((props, ref) => {
                         <span>Settings</span>
                     </button>
                 </Link>
+
+                <button type="button" className="p-link layout-topbar-button" onClick={handleLogOut}>
+                    <i className="layout-menuitem-icon pi pi-fw pi-sign-in"></i>
+                </button>
             </div>
         </div>
     );
