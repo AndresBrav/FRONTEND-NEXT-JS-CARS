5921063f119e84fc7bf29b9777fb188023a72810
Andres Bravo <bravoandres1706@gmail.com>
2025-06-03 13:17:51 -0400

    basic design

--- layout/AppTopbar.tsx
+++ layout/AppTopbar.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable @next/next/no-img-element */
-
+'use client'
 import Link from 'next/link';
 import { classNames } from 'primereact/utils';
 import React, { forwardRef, useContext, useImperativeHandle, useRef } from 'react';
