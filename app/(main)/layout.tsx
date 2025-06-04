e4600f5e8ada19613c3638ab98cdd2c4fa28791b
Andres Bravo <bravoandres1706@gmail.com>
2025-06-04 14:18:32 -0400

    crud optimized

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -6,7 +6,7 @@ interface AppLayoutProps {
 }
 
 export const metadata: Metadata = {
-    title: 'PrimeReact Sakai',
+    title: 'Cars',
     description: 'The ultimate collection of design-agnostic, flexible and accessible React UI Components.',
     robots: { index: false, follow: false },
     viewport: { initialScale: 1, width: 'device-width' },
@@ -18,9 +18,9 @@ export const metadata: Metadata = {
         images: ['https://www.primefaces.org/static/social/sakai-react.png'],
         ttl: 604800
     },
-    icons: {
-        icon: '/favicon.ico'
-    }
+    // icons: {
+    //     icon: '/favicon.ico'
+    // }
 };
 
 export default function AppLayout({ children }: AppLayoutProps) {
--- layout/AppMenu.tsx
+++ layout/AppMenu.tsx
@@ -23,32 +23,31 @@ const AppMenu = () => {
                 { label: 'Add User', icon: 'pi pi-fw pi-id-card', to: '/uikit/users/adduser' },
                 { label: 'Update User', icon: 'pi pi-fw pi-id-card', to: '/uikit/users/updateuser' },
                 { label: 'Delete User', icon: 'pi pi-fw pi-id-card', to: '/uikit/users/deleteuser' },
-                { label: 'Details Cars ', icon: 'pi pi-fw pi-id-card', to: '/uikit/cars/getcars' }, 
-                { label: 'List Files ', icon: 'pi pi-fw pi-id-card', to: '/uikit/cars/files/listfiles' }, 
-                { label: 'Form Layout', icon: 'pi pi-fw pi-id-card', to: '/uikit/formlayout' },
-                { label: 'Input', icon: 'pi pi-fw pi-check-square', to: '/uikit/input' },
-                { label: 'Float Label', icon: 'pi pi-fw pi-bookmark', to: '/uikit/floatlabel' },
-                { label: 'Invalid State', icon: 'pi pi-fw pi-exclamation-circle', to: '/uikit/invalidstate' },
-                { label: 'Button', icon: 'pi pi-fw pi-mobile', to: '/uikit/button', class: 'rotated-icon' },
-                { label: 'Table', icon: 'pi pi-fw pi-table', to: '/uikit/table' },
-                { label: 'List', icon: 'pi pi-fw pi-list', to: '/uikit/list' },
-                { label: 'Tree', icon: 'pi pi-fw pi-share-alt', to: '/uikit/tree' },
-                { label: 'Panel', icon: 'pi pi-fw pi-tablet', to: '/uikit/panel' },
-                { label: 'Overlay', icon: 'pi pi-fw pi-clone', to: '/uikit/overlay' },
-                { label: 'Media', icon: 'pi pi-fw pi-image', to: '/uikit/media' },
-                { label: 'Menu', icon: 'pi pi-fw pi-bars', to: '/uikit/menu', preventExact: true },
-                { label: 'Message', icon: 'pi pi-fw pi-comment', to: '/uikit/message' },
-                { label: 'File', icon: 'pi pi-fw pi-file', to: '/uikit/file' },
+                { label: 'Details Cars ', icon: 'pi pi-fw pi-id-card', to: '/uikit/cars/getcars' },
+                { label: 'List Files ', icon: 'pi pi-fw pi-id-card', to: '/uikit/cars/files/listfiles' }
+                // { label: 'Form Layout', icon: 'pi pi-fw pi-id-card', to: '/uikit/formlayout' },
+                // { label: 'Input', icon: 'pi pi-fw pi-check-square', to: '/uikit/input' },
+                // { label: 'Float Label', icon: 'pi pi-fw pi-bookmark', to: '/uikit/floatlabel' },
+                // { label: 'Invalid State', icon: 'pi pi-fw pi-exclamation-circle', to: '/uikit/invalidstate' },
+                // { label: 'Button', icon: 'pi pi-fw pi-mobile', to: '/uikit/button', class: 'rotated-icon' },
+                // { label: 'Table', icon: 'pi pi-fw pi-table', to: '/uikit/table' },
+                // { label: 'List', icon: 'pi pi-fw pi-list', to: '/uikit/list' },
+                // { label: 'Tree', icon: 'pi pi-fw pi-share-alt', to: '/uikit/tree' },
+                // { label: 'Panel', icon: 'pi pi-fw pi-tablet', to: '/uikit/panel' },
+                // { label: 'Overlay', icon: 'pi pi-fw pi-clone', to: '/uikit/overlay' },
+                // { label: 'Media', icon: 'pi pi-fw pi-image', to: '/uikit/media' },
+                // { label: 'Menu', icon: 'pi pi-fw pi-bars', to: '/uikit/menu', preventExact: true },
+                // { label: 'Message', icon: 'pi pi-fw pi-comment', to: '/uikit/message' },
+                // { label: 'File', icon: 'pi pi-fw pi-file', to: '/uikit/file' },
                 // { label: 'Chart', icon: 'pi pi-fw pi-chart-bar', to: '/uikit/charts' },
                 // { label: 'Misc', icon: 'pi pi-fw pi-circle', to: '/uikit/misc' }
             ]
         },
         {
-            label: 'Pages',
+            label: 'Login',
             icon: 'pi pi-fw pi-briefcase',
             to: '/pages',
             items: [
-                
                 {
                     label: 'Auth',
                     icon: 'pi pi-fw pi-user',
@@ -57,44 +56,44 @@ const AppMenu = () => {
                             label: 'Login',
                             icon: 'pi pi-fw pi-sign-in',
                             to: '/auth/login'
-                        },
-                        {
-                            label: 'Error',
-                            icon: 'pi pi-fw pi-times-circle',
-                            to: '/auth/error'
-                        },
-                        {
-                            label: 'Access Denied',
-                            icon: 'pi pi-fw pi-lock',
-                            to: '/auth/access'
                         }
+                        // {
+                        //     label: 'Error',
+                        //     icon: 'pi pi-fw pi-times-circle',
+                        //     to: '/auth/error'
+                        // },
+                        // {
+                        //     label: 'Access Denied',
+                        //     icon: 'pi pi-fw pi-lock',
+                        //     to: '/auth/access'
+                        // }
                     ]
-                },
-                {
-                    label: 'Crud',
-                    icon: 'pi pi-fw pi-pencil',
-                    to: '/pages/crud'
-                },
-                {
-                    label: 'Not Found',
-                    icon: 'pi pi-fw pi-exclamation-circle',
-                    to: '/pages/notfound'
-                },
-                {
-                    label: 'Empty',
-                    icon: 'pi pi-fw pi-circle-off',
-                    to: '/pages/empty'
                 }
+                // {
+                //     label: 'Crud',
+                //     icon: 'pi pi-fw pi-pencil',
+                //     to: '/pages/crud'
+                // },
+                // {
+                //     label: 'Not Found',
+                //     icon: 'pi pi-fw pi-exclamation-circle',
+                //     to: '/pages/notfound'
+                // },
+                // {
+                //     label: 'Empty',
+                //     icon: 'pi pi-fw pi-circle-off',
+                //     to: '/pages/empty'
+                // }
             ]
         },
         {
             label: 'Get Started',
             items: [
-                {
-                    label: 'Documentation',
-                    icon: 'pi pi-fw pi-question',
-                    to: '/documentation'
-                },
+                // {
+                //     label: 'Documentation',
+                //     icon: 'pi pi-fw pi-question',
+                //     to: '/documentation'
+                // },
                 {
                     label: 'View Source',
                     icon: 'pi pi-fw pi-search',
@@ -109,7 +108,11 @@ const AppMenu = () => {
         <MenuProvider>
             <ul className="layout-menu">
                 {model.map((item, i) => {
-                    return !item?.seperator ? <AppMenuitem item={item} root={true} index={i} key={item.label} /> : <li className="menu-separator"></li>;
+                    return !item?.seperator ? (
+                        <AppMenuitem item={item} root={true} index={i} key={item.label} />
+                    ) : (
+                        <li className="menu-separator"></li>
+                    );
                 })}
             </ul>
         </MenuProvider>
