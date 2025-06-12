5f1bd2b2f0fa5f082174dc1734f79caa5f87f7a3
Andres Bravo <bravoandres1706@gmail.com>
2025-06-12 14:57:27 -0400

    dataset advanced v1

--- app/(main)/uikit/charts/page.tsx
+++ app/(main)/uikit/charts/page.tsx
@@ -16,6 +16,15 @@ type statesdb = {
     state: number;
 };
 
+type DatasetType = {
+    label: string;
+    data: number[];
+    fill: boolean;
+    backgroundColor: string;
+    borderColor: string;
+    tension: number;
+};
+
 const ChartDemo = () => {
     const [options, setOptions] = useState<ChartOptionsState>({});
     const [data, setChartData] = useState<ChartDataState>({});
@@ -25,10 +34,31 @@ const ChartDemo = () => {
     const { layoutConfig } = useContext(LayoutContext);
     const { keyAccess } = useContext(TokenContext); // Trae el token
 
+    const [datasetsManual, setDatasetsManual] = useState<DatasetType[]>([]);
+
     useEffect(() => {
-        console.log('the aray is .....');
-        console.log(array);
+        // console.log('the array is .....');
+        // console.log(datasetsManual);
+        fillArray(array);
     }, [array]);
+
+    const fillArray = (array: statesdb[]) => {
+        console.log('the array is .....');
+        console.log(array);
+    };
+
+    // useEffect(() => {
+    //     const newDataset = {
+    //         label: 'Car',
+    //         data: [25, 50],
+    //         fill: false,
+    //         backgroundColor: '#00bcd4',
+    //         borderColor: '#00bcd4',
+    //         tension: 0.4
+    //     };
+
+    //     setDatasetsManual((prev) => [...prev, newDataset]);
+    // }, []);
 
     useEffect(() => {
         if (!keyAccess || keyAccess.trim() === '') return;
@@ -46,91 +76,40 @@ const ChartDemo = () => {
         fetchData();
     }, [keyAccess]);
 
-    // useEffect(() => {
-    //     const documentStyle = getComputedStyle(document.documentElement);
-    //     const textColor = documentStyle.getPropertyValue('--text-color') || '#495057';
-    //     const textColorSecondary = documentStyle.getPropertyValue('--text-color-secondary') || '#6c757d';
-    //     const surfaceBorder = documentStyle.getPropertyValue('--surface-border') || '#dfe7ef';
-
-    //     const lineData: ChartData = {
-    //         labels: ['1', '2', '3'],
-    //         datasets: [
-    //             {
-    //                 label: 'First Carro1.pdf',
-    //                 data: [80],
-    //                 fill: false,
-    //                 backgroundColor: documentStyle.getPropertyValue('--primary-500') || '#6366f1',
-    //                 borderColor: documentStyle.getPropertyValue('--primary-500') || '#6366f1',
-    //                 tension: 0.4
-    //             },
-    //             {
-    //                 label: 'Second Dataset',
-    //                 data: [28, 48, 40],
-    //                 fill: false,
-    //                 backgroundColor: documentStyle.getPropertyValue('--primary-200') || '#bcbdf9',
-    //                 borderColor: documentStyle.getPropertyValue('--primary-200') || '#bcbdf9',
-    //                 tension: 0.4
-    //             },
-    //             {
-    //                 label: 'Carro2.txt',
-    //                 data: [10, 10],
-    //                 fill: false,
-    //                 backgroundColor: documentStyle.getPropertyValue('--primary-200') || '#bcbdf9',
-    //                 borderColor: documentStyle.getPropertyValue('--primary-200') || '#bcbdf9',
-    //                 tension: 0.4
-    //             },
-    //             {
-    //                 label: 'Carro2.txt',
-    //                 data: [70, 70],
-    //                 fill: false,
-    //                 backgroundColor: documentStyle.getPropertyValue('--primary-200') || '#bcbdf9',
-    //                 borderColor: documentStyle.getPropertyValue('--primary-200') || '#bcbdf9',
-    //                 tension: 0.4
-    //             }
-    //         ]
-    //     };
-
-    //     const lineOptions: ChartOptions = {
-    //         plugins: {
-    //             legend: {
-    //                 labels: {
-    //                     color: textColor
-    //                 }
-    //             }
-    //         },
-    //         scales: {
-    //             x: {
-    //                 ticks: {
-    //                     color: textColorSecondary
-    //                 },
-    //                 grid: {
-    //                     color: surfaceBorder
-    //                 },
-    //                 border: {
-    //                     display: false
-    //                 }
-    //             },
-    //             y: {
-    //                 ticks: {
-    //                     color: textColorSecondary
-    //                 },
-    //                 grid: {
-    //                     color: surfaceBorder
-    //                 },
-    //                 border: {
-    //                     display: false
-    //                 }
-    //             }
-    //         }
-    //     };
-
-    //     setOptions({
-    //         lineOptions
-    //     });
-    //     setChartData({
-    //         lineData
-    //     });
-    // }, [layoutConfig]);
+    // const datasetsManual = [
+    //     {
+    //         label: 'First Carro1.pdf',
+    //         data: [80],
+    //         fill: false,
+    //         backgroundColor: '#6366f1',
+    //         borderColor: '#6366f1',
+    //         tension: 0.4
+    //     },
+    //     {
+    //         label: 'Second Dataset',
+    //         data: [28, 48, 40],
+    //         fill: false,
+    //         backgroundColor: '#bcbdf9',
+    //         borderColor: '#bcbdf9',
+    //         tension: 0.4
+    //     },
+    //     {
+    //         label: 'Carro2.txt',
+    //         data: [10, 10],
+    //         fill: false,
+    //         backgroundColor: '#bcbdf9',
+    //         borderColor: '#bcbdf9',
+    //         tension: 0.4
+    //     },
+    //     {
+    //         label: 'Carro2.txt',
+    //         data: [70, 70],
+    //         fill: false,
+    //         backgroundColor: '#bcbdf9',
+    //         borderColor: '#bcbdf9',
+    //         tension: 0.4
+    //     }
+    // ];
 
     const generarGrafico = () => {
         const documentStyle = getComputedStyle(document.documentElement);
@@ -140,40 +119,7 @@ const ChartDemo = () => {
 
         const lineData: ChartData = {
             labels: ['1', '2', '3'],
-            datasets: [
-                {
-                    label: 'First Carro1.pdf',
-                    data: [80],
-                    fill: false,
-                    backgroundColor: documentStyle.getPropertyValue('--primary-500') || '#6366f1',
-                    borderColor: documentStyle.getPropertyValue('--primary-500') || '#6366f1',
-                    tension: 0.4
-                },
-                {
-                    label: 'Second Dataset',
-                    data: [28, 48, 40],
-                    fill: false,
-                    backgroundColor: documentStyle.getPropertyValue('--primary-200') || '#bcbdf9',
-                    borderColor: documentStyle.getPropertyValue('--primary-200') || '#bcbdf9',
-                    tension: 0.4
-                },
-                {
-                    label: 'Carro2.txt',
-                    data: [10, 10],
-                    fill: false,
-                    backgroundColor: documentStyle.getPropertyValue('--primary-200') || '#bcbdf9',
-                    borderColor: documentStyle.getPropertyValue('--primary-200') || '#bcbdf9',
-                    tension: 0.4
-                },
-                {
-                    label: 'Carro2.txt',
-                    data: [70, 70],
-                    fill: false,
-                    backgroundColor: documentStyle.getPropertyValue('--primary-200') || '#bcbdf9',
-                    borderColor: documentStyle.getPropertyValue('--primary-200') || '#bcbdf9',
-                    tension: 0.4
-                }
-            ]
+            datasets: datasetsManual
         };
 
         const lineOptions: ChartOptions = {
@@ -216,7 +162,7 @@ const ChartDemo = () => {
 
     return (
         <>
-            <div className='col-6 xl:col-6'>
+            <div className="col-6 xl:col-6">
                 <button onClick={generarGrafico} className="p-button p-component p-button-primary mb-3">
                     Generar gráfico
                 </button>
